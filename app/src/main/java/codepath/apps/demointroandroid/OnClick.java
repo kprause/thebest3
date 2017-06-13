b8b9c93fface94fea74c0554eafa4d9a23e98546
kprause <kprause@gmx.net>
2017-06-13 13:58:18 +0200

    Welcomescreen

---
+++ app/src/main/java/codepath/apps/demointroandroid/OnClick.java
@@ -0,0 +1,14 @@
+
+//package codepath.apps.demointroandroid;
+/*
+/**
+ * Created by PierreLaptop on 12.06.2017.
+ */
+
+/* public class OnClick {
+
+    public void yourMethodName(View v){
+        startActivity(new Intent(YourCurrentActivity.this, YourNewActivity.class));
+    }
+}
+*/
